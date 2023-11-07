7c0dd2adb13d5db824ac9647171105850b6150be
Hrutesh Ghodmare <hruteshghodmare19@gmail.com>
2023-11-07 20:49:32 +0530

    Folder Added

---
+++ src/app/employee.ts
@@ -0,0 +1,6 @@
+export class Employee {
+    
+        empName: String = '';
+        empSalary = 0;
+      }
+
---
+++ src/app/photos/photos.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-photos',
+  templateUrl: './photos.component.html',
+  styleUrls: ['./photos.component.css']
+})
+export class PhotosComponent {
+
+
+   image1 = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQVcBkSzTQh8duSduSGvMxvpzyjy00zrEitaw&usqp=CAU"; 
+  image2 = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTkn1x3XELoDf8hV8XbcjCDPt7Al9a0LCOiJA&usqp=CAU";
+ /*  image3 = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTr5Bwbf2unDLRYaQTLkYxvtRNDEgbEx3Q-_EMzarIggoTTtiiJM7177NbMJvcxD_o2BOE&usqp=CAU";  */
+
+}
